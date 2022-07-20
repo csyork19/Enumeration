4c8b13dcb0ade2205b595d963b12b8a06c61ff28
csyork <csyork19@gmail.com>
2022-07-19 22:34:27 -0400

    Update linux privesc manual enumeration checks

--- linux/manual-privesc-enumeration.sh
+++ linux/manual-privesc-enumeration.sh
@@ -6,22 +6,14 @@ hostname
 printf "\n----Operating system----\n\n"
 cat /etc/issue
 
-printf "\n----Running id----\n\n"
-id
-
-printf "----Running sudo -l ----":
-sudo -l
-
 printf "\n----Kernal info----\n\n"
 uname -a
-
 
 printf "\n---- Printing writable directories for the current user ----\n\n"
 find / -type d -writable -print
 
 printf "\n---- Printing writable files for the current user ----\n\n"
 find / -type f -writable -print
-
 
 printf "\n---- Printing world executble directories for the current user ----\n\n"
 find / -perm -o x -type d 2>/dev/null
@@ -34,7 +26,18 @@ find / -perm -o x -type d 2>/dev/null
 printf "\n*---- Print files with SUID bit set ----\n\n"
 find / -perm -u=s -type f 2>/dev/null
 
+
+printf "\n*---- Print files with SGID bit set ----\n\n"
+find / -perm -g=s -type f 2>/dev/null
+
 printf "\n---- Cron Jobs ----\n\n"
 cat /etc/crontab
 
+
+printf "\n---- PATH Variable ----\n\n"
+echo $PATH
+
+
+printf "\n---- Network File Sharing info ----\n\n"
+cat /etc/exports
 
